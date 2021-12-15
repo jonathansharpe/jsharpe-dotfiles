3ccff68f7a8f8411d1d21649cc20a952e7deb426
sharpej1 <jonathansharpe54@gmail.com>
2021-12-15 00:27:13 -0800

    updated neovim config

--- .config/nvim/lua/sharpe-plugins.lua
+++ .config/nvim/lua/sharpe-plugins.lua
@@ -7,16 +7,75 @@ end
 
 return require('packer').startup(function()
     -- Packer
-    use {                                           -- plugins
-      'wbthomason/packer.nvim'
+    use { -- plugins
+        'wbthomason/packer.nvim'
+    }
+	use {
+		'windwp/nvim-autopairs',
+		config = function() 
+			require('nvim-autopairs').setup{}
+		end
+	}
+	use {
+		'akinsho/bufferline.nvim',
+		requires = 'kyazdani42/nvim-web-devicons'
+	}
+	use {
+		'ms-jpq/coq_nvim',
+		branch = 'coq'
+	}
+	use {
+		'ms-jpq/coq.artifacts',
+		branch = 'artifacts'
+	}
+	use {
+		'ms-jpq/coq.thirdparty',
+		branch = '3p'
+	}
+	use {
+		'tpope/vim-surround'
+	}
+    use { -- lualine
+       'hoob3rt/lualine.nvim',
+       requires = {'kyazdani42/nvim-web-devicons', opt = true}
+    }
+    use {                                                         -- tpope: Comments
+        'tpope/vim-commentary'
+    }
+    use { -- Icons for each entry in the completion menu
+        "onsails/lspkind-nvim",
+    }
+    use { -- markdown preview for notes
+       'iamcco/markdown-preview.nvim', 
+       run = 'cd app && yarn install'
+    }
+    use { -- for LaTeX documents, might remove at some point
+        'lervag/vimtex'
+    }
+    -- use {
+    --     'vim-airline/vim-airline'
+    -- }
+    -- use {
+    --     'vim-airline/vim-airline-themes'
+    -- }
+    use {                                                         -- code snippits
+        "L3MON4D3/LuaSnip",
+        -- "hrsh7th/vim-vsnip",
+        -- "rafamadriz/friendly-snippets",
+    }
+    use { -- for deep ocean color scheme
+        'marko-cerovac/material.nvim'
+    }
+    -- use { -- to make the window background transparent
+    --     'xiyaowong/nvim-transparent'
+    -- }
+    use { -- highlights hex color codes in their respective color
+        'norcalli/nvim-colorizer.lua'
+    }
+    use { -- Smooth Scrolling
+        "karb94/neoscroll.nvim",
     }
     use {
-	'lervag/vimtex'
+        'stevearc/gkeep.nvim', run = ':UpdateRemotePlugins'
     }
-	use {
-		'vim-airline/vim-airline'
-	}
-	use {
-		'vim-airline/vim-airline-themes'
-	}
 end)
