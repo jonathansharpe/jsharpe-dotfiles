053596d13659c5b57cf2482ebb2e1d47c0acdf6d
sharpej1 <jonathansharpe54@gmail.com>
2022-06-23 15:51:31 -0700

    updated rofi config

--- .config/rofi/launchers/colorful/app-launcher.sh
+++ .config/rofi/launchers/colorful/app-launcher.sh
@@ -11,12 +11,12 @@
 # style_1     style_2     style_3     style_4     style_5     style_6
 # style_7     style_8     style_9     style_10    style_11    style_12
 
-theme="style_1"
+theme="style_7"
 dir="$HOME/.config/rofi/launchers/colorful"
 
 # dark
 ALPHA="#00000000"
-BG="#000000ff"
+BG="#00000066"
 FG="#FFFFFFff"
 SELECT="#101010ff"
 
@@ -29,7 +29,7 @@ SELECT="#101010ff"
 # accent colors
 COLORS=('#EC7875' '#61C766' '#FDD835' '#42A5F5' '#BA68C8' '#4DD0E1' '#00B19F' \
 		'#FBC02D' '#E57C46' '#AC8476' '#6D8895' '#EC407A' '#B9C244' '#6C77BB')
-ACCENT="${COLORS[$(( $RANDOM % 14 ))]}ff"
+ACCENT="#4dd0e1ff"
 
 # overwrite colors file
 cat > $dir/colors.rasi <<- EOF
