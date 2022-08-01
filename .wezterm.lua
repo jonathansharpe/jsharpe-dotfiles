37f3d5dd68d5d5f15a8b9918b6285283ae678afe
sharpej1 <jonathansharpe54@gmail.com>
2022-07-31 18:45:19 -0700

    updated wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,12 +1,13 @@
 local wezterm = require 'wezterm'
 
 return {
+	font = wezterm.font('Iosevka Nerd Font Mono', {weight = 'Medium'}),
 	default_prog = {"/usr/bin/zsh", "-l"},
 	color_scheme = "Abernathy",
 	window_background_opacity = .85,
 	font_size = 10.0,
 	window_frame = {
-		font = wezterm.font({family="Inter", weight="Medium"}),
+		font = wezterm.font({family="Dosis", weight="Medium"}),
 		font_size = 10.0,
 	}
 }
