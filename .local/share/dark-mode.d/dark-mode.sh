59fd30cd5d6bd815ba2be78cae2bfc0e884604be
sharpej1 <jonathansharpe54@gmail.com>
2022-10-23 13:48:06 -0700

    updated dark mode script

--- .local/share/dark-mode.d/dark-mode.sh
+++ .local/share/dark-mode.d/dark-mode.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-kvantummanager --set Catppuccin-Macchiato-Blue
-plasma-apply-colorscheme CatppuccinMacchiato
-plasma-apply-desktoptheme Scratchy
-/usr/lib/plasma-changeicons Newaita-reborn-dracula-dark
+kvantummanager --set Catppuccin-Macchiato-Blue && notify-send "kvantum theme changed"
+plasma-apply-colorscheme CatppuccinMacchiato && notify-send "plasma colors changed"
+plasma-apply-desktoptheme Scratchy && notify-send "plasma theme changed"
+/usr/lib/plasma-changeicons Newaita-reborn-dracula-dark && notify-send "icons changed"
