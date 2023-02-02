4511e750465207b36bafda9ef9b9c96e3de0f380
Jonathan Sharpe <jonathansharpe54@gmail.com>
2023-02-01 21:55:06 -0800

    updated darkman scripts

--- .local/share/dark-mode.d/dark-mode.sh
+++ .local/share/dark-mode.d/dark-mode.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-kvantummanager --set Catppuccin-Macchiato-Blue && notify-send "kvantum theme changed"
-plasma-apply-colorscheme CatppuccinMacchiato && notify-send "plasma colors changed"
-plasma-apply-desktoptheme Scratchy && notify-send "plasma theme changed"
-/usr/lib/plasma-changeicons Newaita-reborn-dracula-dark && notify-send "icons changed"
+kvantummanager --set Relax-Kvantum && notify-send "kvantum theme changed"
+plasma-apply-colorscheme AritimDark && notify-send "plasma colors changed"
+plasma-apply-desktoptheme Aritim-Dark-Rounded-Blur && notify-send "plasma theme changed"
+/usr/lib/plasma-changeicons Newaita-reborn-dark && notify-send "icons changed"
--- .local/share/light-mode.d/light-mode.sh
+++ .local/share/light-mode.d/light-mode.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-kvantummanager --set Catppuccin-Latte-Blue && notify-send "kvantum theme changed"
-plasma-apply-colorscheme CatppuccinLatte && notify-send "plasma colors changed"
-plasma-apply-desktoptheme Itchy && notify-send "plasma theme changed"
-/usr/lib/plasma-changeicons Newaita-reborn-dracula && notify-send "icons changed"
+kvantummanager --set Relax-Light-Kvantum && notify-send "kvantum theme changed"
+plasma-apply-colorscheme AritimLight && notify-send "plasma colors changed"
+plasma-apply-desktoptheme Aritim-Light-Rounded-Blur && notify-send "plasma theme changed"
+/usr/lib/plasma-changeicons Newaita-reborn && notify-send "icons changed"
