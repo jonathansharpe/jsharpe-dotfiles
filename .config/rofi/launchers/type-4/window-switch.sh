edd59615747e9213ff68b46ec6a7b2ba3323d38e
Jonathan Sharpe <jonathansharpe54@gmail.com>
2023-03-09 14:18:42 -0800

    updated rofi config

--- .config/rofi/launchers/type-4/window-switch.sh
+++ .config/rofi/launchers/type-4/window-switch.sh
@@ -9,10 +9,9 @@
 #
 ## style-1     style-2     style-3     style-4     style-5
 ## style-6     style-7     style-8     style-9     style-10
-## style-11    style-12    style-13    style-14    style-15
 
-dir="$HOME/.config/rofi/launchers/type-2"
-theme='style-2'
+dir="$HOME/.config/rofi/launchers/type-4"
+theme='style-4'
 
 ## Run
 rofi \
