bcbcd2759940bd78bc1a50d9969f0c5fb921bc43
sharpej1 <jsharpe@spu.edu>
2023-03-23 12:05:16 -0700

    updated .local

--- .local/share/dark-mode.d/dark-mode.sh
+++ .local/share/dark-mode.d/dark-mode.sh
@@ -2,5 +2,5 @@
 
 kvantummanager --set Relax-Kvantum && notify-send "kvantum theme changed"
 plasma-apply-colorscheme AritimDark && notify-send "plasma colors changed"
-plasma-apply-desktoptheme Aritim-Dark-Rounded-Blur && notify-send "plasma theme changed"
-/usr/lib/plasma-changeicons Newaita-reborn-dark && notify-send "icons changed"
+# plasma-apply-desktoptheme Aritim-Dark-Rounded-Blur && notify-send "plasma theme changed"
+/usr/lib/plasma-changeicons Rowaita-Cyan-Dark && notify-send "icons changed"
